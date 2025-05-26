efc68262bfe6fc8fa74390b255acdf6926ef6c61
Mahesh Reddy <noreply.github.com>
2025-05-26 23:40:02 +0530

    quick fix

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9eef8ef1._.css",
-  "static/chunks/_6d1ffeb1._.js"
-],
-    source: "dynamic"
-});
